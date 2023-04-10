539cb34dc501b200e764a99524be93120ba24f7c
Matheus Lopes <matlopes96@outlook.com>
2023-04-10 14:03:46 -0300

    updated container padding left

--- src/components/pokemons-list/styles.jsx
+++ src/components/pokemons-list/styles.jsx
@@ -1,10 +1,10 @@
 import styled from "styled-components"
 
 const PokemonList = styled.ul`
-    margin: 60px 30px 0 120px;
+    margin: 60px 30px 0 0;
     display: flex;
     flex-wrap: wrap;
-    justify-content: space-around;
+    justify-content: space-between;
 `
 
 export {PokemonList}
