08ec344caf087da2d0925cd5573d25952e175706
Matheus Alencar <matlopes96@outlook.com>
2023-05-22 19:12:39 -0300

    adding more responsivity to favorite toggle

--- src/components/pokemon/index.jsx
+++ src/components/pokemon/index.jsx
@@ -1,45 +1,48 @@
-import {FavoritePokemonContainer, LisItemLink, ListItemContainer, ListItemDescription, ListItemImage, ListItemName} from "./styles"
+import { FavoritePokemonContainer, LisItemLink, ListItemContainer, ListItemDescription, ListItemImage, ListItemName } from "./styles"
 import { Link } from "react-router-dom"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faStar } from '@fortawesome/free-solid-svg-icons'
-import { useContext } from "react"
+import { useContext, useState, useEffect } from "react"
 import { FavoritesContext } from "../../contexts/favorites-provider"
 
 export const Pokemon = (props) => {
   const { favoritePokemonsIds, toggleFavorite } = useContext(FavoritesContext)
+  const [isFavorite, setIsFavorite] = useState(false)
+
+  useEffect(() => {
+    setIsFavorite(favoritePokemonsIds.includes(props.id))
+  }, [favoritePokemonsIds, props.id])
+
+  const handleToggleFavorite = () => {
+    toggleFavorite(props.id)
+    setIsFavorite(!isFavorite)
+  }
 
   return (
-          <ListItemContainer>
-            <FavoritePokemonContainer>
-              <FontAwesomeIcon
-                icon={faStar}
-                style={
-                  {color: "var(--button-background-color)"}
-                }
-                onClick={() => toggleFavorite(props.id)}
-              />
-            </FavoritePokemonContainer>
+    <ListItemContainer>
+      <FavoritePokemonContainer>
+        <FontAwesomeIcon
+          icon={faStar}
+          style={{
+            color: isFavorite ? "yellow" : "var(--button-background-color)"
+          }}
+          onClick={handleToggleFavorite}
+        />
+      </FavoritePokemonContainer>
 
-            <Link
-              to={`/pokemon/${props.id}`}
-              onClick={
-                () => {
-                  localStorage.setItem("pageCounter", props.listPage);
-
-                  console.log(favoritePokemonsIds);
-                }
-              }
-            >
-              <LisItemLink>
-                <ListItemImage image={props.image} />
-
-                <ListItemName>{props.name}</ListItemName>
-
-                <ListItemDescription>
-                  {props.description}
-                </ListItemDescription>
-              </LisItemLink>
-            </Link>
-          </ListItemContainer>
-    )
+      <Link
+        to={`/pokemon/${props.id}`}
+        onClick={() => {
+          localStorage.setItem("pageCounter", props.listPage)
+          console.log(localStorage)
+        }}
+      >
+        <LisItemLink>
+          <ListItemImage image={props.image} />
+          <ListItemName>{props.name}</ListItemName>
+          <ListItemDescription>{props.description}</ListItemDescription>
+        </LisItemLink>
+      </Link>
+    </ListItemContainer>
+  )
 }
--- src/contexts/favorites-provider.jsx
+++ src/contexts/favorites-provider.jsx
@@ -1,35 +1,41 @@
 import { createContext, useState } from "react";
 
-const FavoritesContext = createContext()
+const FavoritesContext = createContext();
 
-const FavoritesProvider = (props, pokemonId) => {  
-    const [favoritePokemonsIds, setFavoritePokemonsIds] = useState([]);
+const FavoritesProvider = (props) => {
+  const [favoritePokemonsIds, setFavoritePokemonsIds] = useState([]);
 
-    const toggleFavorite = (pokemonId) => {
-        if (favoritePokemonsIds.includes(pokemonId)) {
-          console.log(`pokemon ${pokemonId} retirado dos favoritos`);
-    
-          setFavoritePokemonsIds(
-            (oldList) => oldList.filter(
-              (id) => id !== pokemonId
-            )
-          )
-    
-          event.target.style.color = "var(--button-background-color)";
-        } else {
-          console.log(`pokemon ${pokemonId} adicionado aos favoritos`);
-    
-          setFavoritePokemonsIds((oldList) => [...oldList, pokemonId]);
-    
-          event.target.style.color = "yellow";
-        }
-      };
+  const toggleFavorite = (pokemonId) => {
+    if (favoritePokemonsIds.includes(pokemonId)) {
+      console.log(`pokemon ${pokemonId} retirado dos favoritos`);
 
-    return (
-        <FavoritesContext.Provider value={{favoritePokemonsIds, toggleFavorite}}>
-            {props.children}
-        </FavoritesContext.Provider>
-    )
-}
+      setFavoritePokemonsIds((oldList) =>
+        oldList.filter((id) => id !== pokemonId)
+      );
 
-export { FavoritesContext, FavoritesProvider }
+      const updatedFavorites = favoritePokemonsIds.filter(
+        (id) => id !== pokemonId
+      );
+
+      localStorage.setItem("favoritesList", JSON.stringify(updatedFavorites));
+    } 
+    
+    else {
+      console.log(`pokemon ${pokemonId} adicionado aos favoritos`);
+
+      setFavoritePokemonsIds((oldList) => [...oldList, pokemonId]);
+
+      const updatedFavorites = [...favoritePokemonsIds, pokemonId];
+
+      localStorage.setItem("favoritesList", JSON.stringify(updatedFavorites));
+    }
+  };
+
+  return (
+    <FavoritesContext.Provider value={{ favoritePokemonsIds, toggleFavorite }}>
+      {props.children}
+    </FavoritesContext.Provider>
+  );
+};
+
+export { FavoritesContext, FavoritesProvider };
