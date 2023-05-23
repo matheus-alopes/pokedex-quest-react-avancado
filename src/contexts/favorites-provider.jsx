a6fe75faa3a4f188a14ea24d9edc809c33257f71
Matheus Lopes <matlopes96@outlook.com>
2023-05-23 14:53:38 -0300

    adding fav list icon color to localstorage check

--- src/contexts/favorites-provider.jsx
+++ src/contexts/favorites-provider.jsx
@@ -3,7 +3,7 @@ import { createContext, useState } from "react";
 const FavoritesContext = createContext();
 
 const FavoritesProvider = (props) => {
-  const [favoritePokemonsIds, setFavoritePokemonsIds] = useState([]);
+  const [favoritePokemonsIds, setFavoritePokemonsIds] = useState(localStorage.favoritesList ? localStorage.favoritesList : []);
 
   const toggleFavorite = (pokemonId) => {
     if (favoritePokemonsIds.includes(pokemonId)) {
