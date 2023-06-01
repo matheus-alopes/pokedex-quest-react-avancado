a028a66896dfe0eb3e686b232c4e8f37be5f14a2
Matheus Lopes <matlopes96@outlook.com>
2023-06-01 12:35:31 -0300

    filter favorite styles more responsive

--- src/components/filter-favorite-section/index.jsx
+++ src/components/filter-favorite-section/index.jsx
@@ -6,17 +6,12 @@ import { useContext } from "react";
 import { FavoritesContext } from "../../contexts/favorites-provider";
 
 export const FilterFavoriteSection = () => {
-    const { favoritePokemonsIds, setFilterFavorites, favoritesPokemonsList, setFavoritesPokemonsList } = useContext(FavoritesContext);
+    const { favoritePokemonsIds, filterFavorites, setFilterFavorites, favoritesPokemonsList, setFavoritesPokemonsList } = useContext(FavoritesContext);
 
-    async function handdleFilterFavorite(event) {
-        const iconContainer = document.getElementById("filter-favorite-container");
-
-        if (event.target.style.color == "white") {
-            event.target.style.color = "rgb(255, 243, 50)"
-            
-            iconContainer.style.backgroundColor = "var(--button-background-color)";
-            iconContainer.style.boxShadow = "var(--box-shadow) 0px 5px 15px";
-
+    async function handdleFilterFavorite() {
+        if (filterFavorites) {
+            setFilterFavorites( () => false);
+        } else {
             console.log(favoritePokemonsIds);
 
             const favoritesDetails = await getFavoritePokemonsDetails(favoritePokemonsIds);
@@ -28,13 +23,6 @@ export const FilterFavoriteSection = () => {
             setFilterFavorites( () => true);
 
             console.log(favoritesPokemonsList)
-        } else {
-            event.target.style.color = "white"
-
-            iconContainer.style.backgroundColor = "rgb(255, 243, 128, 0.7)";
-            iconContainer.style.boxShadow = "none";
-
-            setFilterFavorites( () => false);
         }
     }
 
@@ -42,12 +30,21 @@ export const FilterFavoriteSection = () => {
         <FilterFavoriteContainer>
             <FilterFavoriteTitle>Filter Favorites</FilterFavoriteTitle>
 
-            <FilterFavoriteIcon id="filter-favorite-container">
+            <FilterFavoriteIcon
+                style={
+                    {
+                        backgroundColor: filterFavorites ? "var(--button-background-color)" : "rgb(255, 243, 128, 0.7)",
+                        boxShadow: filterFavorites ? "var(--box-shadow) 0px 5px 15px" : "none"
+                    }
+                }
+            >
                 <FontAwesomeIcon
                     icon={faStar}
-                    style={{
-                        color: "white",
-                    }}
+                    style={
+                        {
+                            color: filterFavorites ? "rgb(255, 243, 50)" : "white"
+                        } 
+                    }
                     onClick={handdleFilterFavorite}
                 />
             </FilterFavoriteIcon>
